77ef1d7b0cd6390a19d91e7f65196beda515ad22
Akpali Francis Ezennaya <124525866+Akpali9@users.noreply.github.com>
2023-11-20 00:36:39 +0100

    Add files via upload

---
+++ autoplay.js
@@ -0,0 +1,14 @@
+const slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+
+function showSlide(n) {
+  slides[currentSlide].classList.remove('active');
+  currentSlide = (n + slides.length) % slides.length;
+  slides[currentSlide].classList.add('active');
+}
+
+function nextSlide() {
+  showSlide(currentSlide + 1);
+}
+
+setInterval(nextSlide, 2000); 
